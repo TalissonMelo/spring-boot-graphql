08786ef833c67402e54568fe51fb4c78a7bf893f
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-10-02 18:28:54 -0300

    Entidade Cliente

---
+++ src/main/java/com/talissonmelo/entity/Client.java
@@ -0,0 +1,13 @@
+package com.talissonmelo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Client {
+	private String name;
+	private String email;
+}
--- src/main/java/com/talissonmelo/query/QueryGraphQL.java
+++ src/main/java/com/talissonmelo/query/QueryGraphQL.java
@@ -3,12 +3,18 @@ package com.talissonmelo.query;
 import org.springframework.stereotype.Component;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.talissonmelo.entity.Client;
+
 
 @Component
 public class QueryGraphQL implements GraphQLQueryResolver{
 	
 	public String hello() {
 		return "GraphQL";
+	}
+	
+	public Client client() {
+		return new Client("Talisson Melo", "talisson.cursos@gmail.com");
 	}
 
 }
