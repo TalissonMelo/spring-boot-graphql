aee7857549f6e0c84937e6224b121275bfd942f7
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-10-02 18:34:18 -0300

    Query com Lista de Clientes

--- src/main/java/com/talissonmelo/query/QueryGraphQL.java
+++ src/main/java/com/talissonmelo/query/QueryGraphQL.java
@@ -1,5 +1,8 @@
 package com.talissonmelo.query;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
@@ -15,6 +18,14 @@ public class QueryGraphQL implements GraphQLQueryResolver{
 	
 	public Client client() {
 		return new Client("Talisson Melo", "talisson.cursos@gmail.com");
+	}
+	
+	public List<Client> clients(){
+		List<Client> list = new ArrayList<>();
+		for(int i =0; i < 10; i++) {
+				list.add(new Client("Cliente " + i , "cliente" + i + "@gmail.com" ));
+		}
+		return list;
 	}
 
 }
