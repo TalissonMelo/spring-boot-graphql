8dfab006eb85d8611e81ef47fe269e30a064e27a
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-10-03 09:07:44 -0300

    Organizando a configuração do schema

---
+++ src/main/java/com/talissonmelo/query/ClientGraphQL.java
@@ -0,0 +1,33 @@
+package com.talissonmelo.query;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.talissonmelo.entity.Client;
+import com.talissonmelo.repository.ClientRepository;
+import com.talissonmelo.service.ClientService;
+
+
+@Component
+public class ClientGraphQL implements GraphQLQueryResolver {
+	
+	@Autowired
+	private ClientRepository clientRepository;
+
+	
+	@Autowired
+	private ClientService clientService;
+	
+	
+	public Client client(Long id) {
+		return clientService.clientById(id);
+	}
+	
+	public List<Client> clients(){
+		return clientRepository.findAll();
+	}
+
+}
